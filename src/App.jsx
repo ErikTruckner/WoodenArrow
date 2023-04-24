f0a12d39f55d045917d0a6b7fa9cb564c785f3b7
Erik Truckner <eriktruckner@comcast.net>
2023-04-24 16:42:53 -0400

    it's a wooden arrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { PlayerOne, Bullet } from './characters/PlayerOne'
+import PlayerOne from './characters/PlayerOne'
 import './index.css'
 import { useState } from 'react'
 import * as THREE from 'three'
 
 function App({ camera }) {
-  const [bullets, setBullets] = useState([])
-
-  const handleMouseDown = (event) => {
-    const mouse = new THREE.Vector2()
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
-
-    const vector = new THREE.Vector3(mouse.x, mouse.y, 0.5).unproject(camera)
-
-    setBullets([...bullets, { position: vector }])
-  }
-
   const divStyle = {
     color: 'red',
     backgroundColor: 'lightblue',
@@ -32,13 +20,11 @@ function App({ camera }) {
   }
   return (
     <>
-      <Canvas onMouseDown={handleMouseDown}>
+      <Canvas>
+        <OrbitControls />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <PlayerOne />
-        {bullets.map((bullet, index) => (
-          <Bullet key={index} position={bullet.position} />
-        ))}
       </Canvas>
       <div style={divStyle}>
         <h1>Project Wooden Arrow</h1>
--- src/characters/PlayerOne.jsx
+++ src/characters/PlayerOne.jsx
@@ -1,53 +1,19 @@
-import { Extrude, Shape } from '@react-three/drei'
-import * as THREE from 'three'
+import { useTexture } from '@react-three/drei'
 
-import woodTexture from '../../public/wood_texture.jpg'
-
-const shape = new THREE.Shape()
-shape.moveTo(0, 1)
-shape.lineTo(-1, -1)
-shape.lineTo(1, -1)
-shape.lineTo(0, 1)
-
-const extrudeSettings = {
-  depth: 0.5,
-  bevelEnabled: false,
-}
-
-const texture = new THREE.TextureLoader().load('path/to/texture.jpg')
-texture.wrapS = texture.wrapT = THREE.RepeatWrapping
-
-const material = new THREE.MeshStandardMaterial({
-  color: 'lightblue',
-  map: texture,
-})
-
-export const PlayerOne = () => {
+const PlayerOne = () => {
+  const woodTexture = useTexture('../../public/wood_texture.jpg')
   return (
-    <Extrude args={[shape, extrudeSettings]}>
-      <mesh material={material} />
-    </Extrude>
+    <group>
+      <mesh>
+        <sphereGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial map={woodTexture} />
+      </mesh>
+      <mesh position={[0, -3.5, 0]}>
+        <cylinderGeometry args={[-0.2, -0.2, 7, 16]} />
+        <meshBasicMaterial map={woodTexture} />
+      </mesh>
+    </group>
   )
 }
 
-export const Bullet = ({ position }) => {
-  const velocity = new THREE.Vector3(0, 5, 0)
-
-  useFrame(({ clock, scene }) => {
-    const elapsed = clock.getElapsedTime()
-    const deltaY = velocity.y * elapsed
-
-    if (position.y + deltaY < 10) {
-      position.y += deltaY
-      const clone = new THREE.Mesh(
-        new THREE.ExtrudeGeometry(shape, extrudeSettings),
-        material
-      )
-      clone.position.copy(position)
-      clone.scale.set(0.125, 0.125, 0.125)
-      scene.add(clone)
-    }
-  })
-
-  return null
-}
+export default PlayerOne
