723c1eeb2c1920bcdd58c019653c1e3083fcbc95
Erik Truckner <eriktruckner@comcast.net>
2023-04-24 14:18:23 -0400

    mapped the ship

--- src/characters/PlayerOne.jsx
+++ src/characters/PlayerOne.jsx
@@ -1,6 +1,8 @@
 import { Extrude, Shape } from '@react-three/drei'
 import * as THREE from 'three'
 
+import woodTexture from '../../public/wood_texture.jpg'
+
 const PlayerOne = () => {
   const shape = new THREE.Shape()
   shape.moveTo(0, 1)
@@ -8,14 +10,34 @@ const PlayerOne = () => {
   shape.lineTo(1, -1)
   shape.lineTo(0, 1)
 
-  const extrudeSettings = {
+  // Define the UV coordinates for the shape
+  const uvGenerator = new THREE.ExtrudeBufferGeometry(shape, {
     depth: 0.5,
     bevelEnabled: false,
-  }
+  })
+  const uvAttribute = uvGenerator.attributes.uv
+
+  // Scale the UV coordinates to fit the texture onto the shape
+  const texture = new THREE.TextureLoader().load(woodTexture)
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  const scale = new THREE.Vector2(1, 1).multiplyScalar(2)
+  const offset = new THREE.Vector2(0.5, 0.5)
+  const matrix = new THREE.Matrix3().set(
+    scale.x,
+    0,
+    offset.x,
+    0,
+    scale.y,
+    offset.y,
+    0,
+    0,
+    1
+  )
+  uvAttribute.applyMatrix3(matrix)
 
   return (
-    <Extrude args={[shape, extrudeSettings]}>
-      <meshStandardMaterial color='lightblue' />
+    <Extrude args={[shape, { depth: 0.5, bevelEnabled: false }]}>
+      <meshBasicMaterial map={texture} />
     </Extrude>
   )
 }
